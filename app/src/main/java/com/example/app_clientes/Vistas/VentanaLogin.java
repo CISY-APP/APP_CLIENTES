0bf03e79583a1ed989172946fd653bca541ed9c4
RaulRs97 <rramosaristos@gmail.com>
2020-05-14 09:08:58 +0200

    Continuacion en clase VentanaLogin, inclusion de atributos y evento onclick e intents a demas ventanas o activities:
    Se deja atributos del xml relacionados con los que tiene la clase,
    ademas se implementa la interfaz de 'onclicklistener', y en su metodo se
    programa para que dependiendo del boton se redireccione a traves de un
    intent explicito a otras ventanas.
    
    Estas ventanas como claveOlvidada, o registro, han sido creadas solo de
    forma basica para poder utilizadas para la redireccion pero nada mas, su
    avance se realizara de manera independiente en otras ramas. Para
    aclarar, se genero el XML y .java basico.
    
    Se modifico el manifest para declarar la existencia de las nuevas
    actividades, y tambien se comento una pequeña parte.
    
    Se modifica el xml de ventana login para hacer que el textview de clave
    olvidad sea clickable.
    
    On branch ventanaLogin
    Changes to be committed:
    	modified:   app/src/main/AndroidManifest.xml
    	new file:   app/src/main/java/com/example/app_clientes/Vistas/VentanaClaveOlvidada.java
    	modified:   app/src/main/java/com/example/app_clientes/Vistas/VentanaLogin.java
    	new file:   app/src/main/java/com/example/app_clientes/Vistas/VentanaRegistro.java
    	new file:   app/src/main/res/layout/activity_ventana_clave_olvidada.xml
    	modified:   app/src/main/res/layout/activity_ventana_login.xml
    	new file:   app/src/main/res/layout/activity_ventana_registro.xml

--- app/src/main/java/com/example/app_clientes/Vistas/VentanaLogin.java
+++ app/src/main/java/com/example/app_clientes/Vistas/VentanaLogin.java
@@ -1,16 +1,66 @@
 package com.example.app_clientes.Vistas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.app_clientes.R;
-
-public class VentanaLogin extends AppCompatActivity {
+//Clase que contiene toda la logica y conexion con la ventana de login de la app:
+public class VentanaLogin extends AppCompatActivity implements View.OnClickListener{
+    //Atributos pertenecientes a la clase
+    private EditText editTextUsuario, editTextClave;
+    private Button btIniciarSesion, btRegistrar;
+    private TextView txtBtClaveOlvidada;
+    //Metodo onCreate encargado de crear la actividad
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ventana_login);
+        //Asociamos los elementos XML a los atributos
+        editTextUsuario=findViewById(R.id.editTextUsuarioInicioSesion);
+        editTextClave=findViewById(R.id.editTextClaveInicioSesion);
+        btIniciarSesion=findViewById(R.id.buttonLoginInicioSesion);
+        btRegistrar=findViewById(R.id.buttonRegistrarInicioSesion);
+        txtBtClaveOlvidada=findViewById(R.id.textViewOlvidasteTuClaveInicioSesion);
+        //Vinculamos los botones al listener del metodo onclick, que esta implementado en esta clase
+        btIniciarSesion.setOnClickListener(this);
+        btRegistrar.setOnClickListener(this);
+
+    }
+    //Metodo onClick implementado de la interfaz View.OnClickListener.
+    @Override
+    public void onClick(View v) {
+        //Segun el boton clickado hacemos lo siguiente:
+        if(v.equals(btIniciarSesion)){
+            //HACER COMPROBACIONES SEGUN DE LOS DATOS LEIDOS EN LOS EDITEXT, Y A TRAVES DE RETROFIT CONSEGUIR LA INFORMACION PARA VALIDARLO
+            //-----------------------------------------------------------------------------------------------------------------------------
+
+
+            //Instanciamos nuestro objeto Intent explicito, ya que en los parametros ponemos que empieza en esta
+            //clase que sera el contexto y que iniciara la clase que se encarga de la otra actividad en este caso.
+            Intent intentInicioSesion = new Intent(this, VentanaPrincipal.class);
+            //Iniciamos la nueva actividad:
+            startActivity(intentInicioSesion);
+        }else if(v.equals(btRegistrar)){
+            //Instanciamos nuestro objeto Intent explicito, ya que en los parametros ponemos que empieza en esta
+            //clase que sera el contexto y que iniciara la clase que se encarga de la otra actividad en este caso.
+            Intent intentRegistro = new Intent(this, VentanaRegistro.class);
+            //Iniciamos la nueva actividad:
+            startActivity(intentRegistro);
+        }else if(v.equals(txtBtClaveOlvidada)){
+            //Instanciamos nuestro objeto Intent explicito, ya que en los parametros ponemos que empieza en esta
+            //clase que sera el contexto y que iniciara la clase que se encarga de la otra actividad en este caso.
+            Intent intentClaveOlvidada = new Intent(this, VentanaClaveOlvidada.class);
+            //Iniciamos la nueva actividad:
+            startActivity(intentClaveOlvidada);
+        }
     }
 }
