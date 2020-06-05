b02e717c4797b225eae4008acaeb24da4d368350
RaulRs97 <rramosaristos@gmail.com>
2020-06-05 09:25:41 +0200

    Retoque final splashScreen y mas:
    Se modifica el manifest para que la actividad del splashScreen no se
    vuelva a crear al girar la pantalla, ya que eso provoca la creacion
    de la actividad de nuevo.
    Se agrega al layout del splash un titulo con el nombre de la app el cual
    se anima tambien con scale.
    
    Se cambia el nombre de la app.
    
    On branch mejoraFinal
    Changes to be committed:
    	modified:   app/src/main/AndroidManifest.xml
    	modified:   app/src/main/java/com/example/app_clientes/splash_screen/Splash_screen.java
    	modified:   app/src/main/res/layout/splash_screen.xml
    	modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/app_clientes/splash_screen/Splash_screen.java
+++ app/src/main/java/com/example/app_clientes/splash_screen/Splash_screen.java
@@ -1,27 +1,34 @@
+//Indicamos a que paquete pertenece esta clase:
 package com.example.app_clientes.splash_screen;
-
+//Importamos los siguientes paquetes:
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
-
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.example.app_clientes.Biblioteca;
 import com.example.app_clientes.R;
 import com.example.app_clientes.vistas.VentanaLogin;
-
+//Clase que muestra el splashscreen de la app al iniciar ademas del nombre animado:
 public class Splash_screen extends AppCompatActivity {
-
+    //Atributos XML:
+    private TextView textViewNombre;
+    //Metodo onCreate encargado de crear la actividad:
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Llamamos al superconstructor y conectamos el XML del login:
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.splash_screen);
-
-        int DURATION_SPLASH = 2800;
+        //Asociamos el elemento XML a el atributo:
+        textViewNombre = findViewById(R.id.textViewNombreSplashScreen);
+        //Duracion del splash:
+        int duracionSplash = 2800;
+        //Lanzamos en un hilo la animacion, y cuando termine se lleva a la ventana de login:
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -29,6 +36,22 @@ public class Splash_screen extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, DURATION_SPLASH);
+        }, duracionSplash);
+        //Animaciones tipo scale despues de que todoo se haya realizado:
+        textViewNombre.post(new Runnable() {
+            @Override
+            public void run() {
+                //Declaramos un animatorSet para poder luego ejecutar un conjunto de animaciones a la vez:
+                AnimatorSet animatorSetEscale = new AnimatorSet();
+                //Animacion para el txtView con el nombre de la app:
+                ObjectAnimator scaleDownX_txtViewNombre = ObjectAnimator.ofFloat(textViewNombre, "scaleX", 0.0f, 1.0f);
+                ObjectAnimator scaleDownY_txtViewNombre = ObjectAnimator.ofFloat(textViewNombre, "scaleY", 0.0f, 1.0f);
+                //Configuramos el animatorSet, como se tienen que reproducir las animaciones, el tiempo que duran, que clase de interpolador utilizan y la lanzamos:
+                animatorSetEscale.play(scaleDownX_txtViewNombre).with(scaleDownY_txtViewNombre);
+                animatorSetEscale.setDuration(Biblioteca.tAnimacionesScaleInicial);
+                animatorSetEscale.setInterpolator(new AccelerateDecelerateInterpolator());
+                animatorSetEscale.start();
+            }
+        });
     }
 }
